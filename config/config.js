a78e6abbad26ba56a92545bbaad93c2d0b3f9fb7
Nanda Safiq Alfiansyah <nandasafiqalfiansyah@gmail.com>
2024-03-09 16:16:23 +0700

    fix : bug database

--- config/config.js
+++ config/config.js
@@ -1,12 +1,7 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-const isProduction = process.env.NODE_ENV === 'production'
-
-const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DATABASE}?sslmode=require`
-
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+  connectionString: process.env.POSTGRES_URL
 })
-
 module.exports = pool
